7846fddb76cc674cfbf9e9a2105c05da92155b02
kaltera <kaltera@gmail.com>
2015-04-22 16:14:19 +0100

    Added Family Number and Aliases

--- Exploring_File.py
+++ Exploring_File.py
@@ -6,12 +6,21 @@ from sets import Set
 
 titanic = pandas.read_csv("train.csv")
 titles = []
+alias = []
 
 for index, row in titanic.iterrows():
     name = row["Name"]
     title = re.split("\.",re.split(",",name)[1])[0].strip().title()
     titles.append(title)
 
-titanic.insert(4,"Title",titles)
+    if "\"" in row["Name"] or "(" in row["Name"]:
+        alias.append("1")
+    else:
+        alias.append("0")
+
+
+titanic.insert(5,"Title",titles)
+titanic.insert(6,"Alias",alias)
+titanic.insert(10,"Fam", titanic["SibSp"] + titanic["Parch"])
 
 print titanic
